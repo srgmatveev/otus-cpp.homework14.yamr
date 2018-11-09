82215424edf0b38d7841d0cebe77f48299e1c857
Sergio <srgmatveev@gmail.com>
2018-11-09 16:48:13 +0300

    add tests

--- tests/utils_test.cpp
+++ tests/utils_test.cpp
@@ -14,20 +14,41 @@ TEST(GetFileSize_TEST, GetFileSize_TEST_File_Size)
     ASSERT_EQ(getFileSize(f_name), 132);
 }
 
-TEST(SplitFile_TEST, SplitFile_Test_on_Exist_True)
+TEST(SplitFilePos_TEST, SplitFilePos_Test_on_Exist_True)
 {
     std::string f_name = "addresses.txt";
     std::size_t parts = 3;
-    ASSERT_NO_THROW(splitFile(f_name, parts));
+    ASSERT_NO_THROW(splitFilePos(f_name, parts));
 }
 
-TEST(SplitFile_TEST, SplitFile_Test_on_Exist_False)
+TEST(SplitFilePos_TEST, SplitFilePos_Test_on_Exist_False)
 {
     std::string f_name = "addresses.txt1";
     std::size_t parts = 3;
-    ASSERT_ANY_THROW(splitFile(f_name, parts));
+    ASSERT_ANY_THROW(splitFilePos(f_name, parts));
 }
 
-TEST(SplitFile_TEST, SplitFile_Test_on_split)
+TEST(SplitFilePos_TEST, SplitFilePos_Test_on_split)
 {
+
+    std::vector<std::streampos> tmp_vec;
+    std::vector<std::streampos> result_vec{0, 94, 132};
+    std::string f_name = "addresses.txt";
+    std::size_t parts = 3;
+    tmp_vec = splitFilePos(f_name, parts);
+    ASSERT_THAT(tmp_vec.size(), 3);
+    for (auto i = 0; i < tmp_vec.size(); ++i)
+    {
+        ASSERT_THAT(tmp_vec.at(i), result_vec.at(i));
+    }
+}
+
+TEST(SplitFilePos_TEST, SplitFilePos_Test_on_empty)
+{
+    std::vector<std::streampos> tmp_vec;
+    std::string f_name = "addr_empty.txt";
+    std::size_t parts = 3;
+    tmp_vec = splitFilePos(f_name, parts);
+    ASSERT_THAT(tmp_vec.size(), 1);
+    ASSERT_THAT(tmp_vec.at(0), 0);
 }
--- utils.h
+++ utils.h
@@ -17,15 +17,44 @@ std::streampos getFileSize(const std::string &fileName)
     return fSize;
 }
 
-std::vector<std::size_t> splitFile(const std::string &fileName, std::size_t partNumbs)
+std::vector<std::streampos> splitFilePos(const std::string &fileName, std::size_t partNumbs = 1)
 {
-    std::vector<std::size_t> partsPos;
-    partsPos.reserve(partNumbs);
+    std::vector<std::streampos> partsPos;
+
     std::ifstream file(fileName, std::ios::in);
     if (!file.is_open())
         throw std::invalid_argument("Error! Couldn't open file " + fileName);
-    return partsPos;
 
-    /*if (!file.size())
-        throw std::invalid_argument("Error! Empty file " + fileName);*/
+    auto fileSize = getFileSize(fileName);
+    if (!partNumbs)
+        partNumbs = 1;
+    auto partSize = fileSize % partNumbs ? fileSize / partNumbs + 1 : fileSize / partNumbs;
+    auto pos = partSize;
+
+    if (fileSize)
+    {
+        partsPos.reserve(partNumbs);
+        partsPos.emplace_back(0);
+        std::string line{""};
+        do
+        {
+            file.seekg(pos, std::ios::beg);
+            std::getline(file, line);
+            pos = file.tellg();
+            pos += partSize;
+            if (pos < fileSize)
+                partsPos.emplace_back(pos);
+            else
+            {
+                partsPos.emplace_back(fileSize);
+                break;
+            }
+        } while (!file.eof());
+    }
+    else
+    {
+        partsPos.emplace_back(0);
+    }
+    file.close();
+    return partsPos;
 }
